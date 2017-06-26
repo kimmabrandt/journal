2839c49e82cc901ed59f08b3b4d7ca2551acfc49
kimmabrandt <kimmabrandt@gmail.com>
2017-06-25 20:14:00 -0700

    finished w. backend - profile auth

---
+++ app-api/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var jwt = require('express-jwt');
+var auth = jwt({
+  secret: 'MY_SECRET',
+  userProperty: 'payload'
+});
+
+// profile router
+// throw error if accessed without jwt
+router.get('/profile', auth, ctrlProfile.profileRead);
+
+// error handlers - catch unauthorized errors
+app.use(function(err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({"message" : err.name + ": " + err.message});
+  }
+});
